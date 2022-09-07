74a2ad65d5861de456448065ce72b4f0135585f1
mingwei <mingwei@caih.com>
2022-09-07 15:15:30 +0800

    next blog

---
+++ _posts/preview copy.md
@@ -0,0 +1,25 @@
+---
+title: '复制的文章'
+excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.'
+coverImage: '/assets/blog/preview/cover.jpg'
+date: '2020-03-16T05:35:07.322Z'
+author:
+  name: Joe Haddad
+  picture: '/assets/blog/authors/joe.jpeg'
+ogImage:
+  url: '/assets/blog/preview/cover.jpg'
+---
+
+## 信息化基础（上）
+信息特性
+* 依附性
+* 随机性
+
+## 信息的质量属性：
+* 精确性
+* 完整性
+* 可靠性
+* 经济性
+* 可验证性
+* 安全性
+传输技术是信息技术的核心。
--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,19 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const Header = () => {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/">
-        <a className="hover:underline">Blog</a>
+        <a className="hover:underline">首页</a>
       </Link>
-      .
+      <Link href="/">
+        <a className="hover:underline ml-8">分类</a>
+      </Link>
+      <Link href="/">
+        <a className="hover:underline ml-8">我的</a>
+      </Link>
     </h2>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
